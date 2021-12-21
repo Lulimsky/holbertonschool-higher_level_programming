be78453d4e3584ac4a4f76a8bd59c089f8d09b6a
lulimsky <3337@holbertonshool.com>
2021-12-21 18:38:50 +0000

    until 7

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for nxd in range(99):
+    print("{} = {}".format(nxd, hex(nxd)))
+
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for doscomb in range(0, 99):
+    print("{:02d}".format(doscomb), end=", ")
+print(99)
+
