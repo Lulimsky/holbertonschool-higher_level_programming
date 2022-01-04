f65b387753eeafef9d5aec21a2bf7622ae630c2f
lulimsky <3337@holbertonshool.com>
2022-01-04 16:49:04 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    index_list = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    index_list.append(i)
-                    index_list.append(j)
-    print(list(set(index_list)))
-    return index_list
+def square_matrix_simple(matrix=[[]]):
+    return [[x ** 2 for x in y] for y in matrix]
