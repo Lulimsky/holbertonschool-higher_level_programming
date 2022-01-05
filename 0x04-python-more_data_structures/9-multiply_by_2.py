d9a441b514ef1c41badc79a47f41d2cd9e6fa09a
lulimsky <3337@holbertonshool.com>
2022-01-05 14:33:10 +0000

    9fix2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     mult = {}
     for key in a_dictionary:
-        new_d[i] = a_dictionary[i] * 2
-    return new_d
+       mult[key] = a_dictionary[key] * 2
+    return mult
