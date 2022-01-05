fada360c65ce00a700cdb3601cddb18bae7c73ec
lulimsky <3337@holbertonshool.com>
2022-01-05 00:19:36 +0000

    4fix

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    od_set = (set(set_2).difference(set_1))
+    od_set = set.union(set_1.difference(set_2), set_2.difference(set_1))
     return od_set
