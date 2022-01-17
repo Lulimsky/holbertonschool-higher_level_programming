d5dcd10622ac952eab10a47f3a8f93fb87a02d38
lulimsky <3337@holbertonshool.com>
2022-01-17 23:34:31 +0000

    task 0_fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in my_list:
         try:
             print("{}".format(my_list[i]), end="")
-        except(TypeError, IndexError):
+            string += 1
+        except (TypeError, IndexError):
             pass
         print()
         return string
