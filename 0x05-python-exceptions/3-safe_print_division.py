03e34eb55fe2b72edf51772b107d46a92ca478b5
lulimsky <3337@holbertonshool.com>
2022-01-18 00:37:16 +0000

    task3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        divide = a / b
+    except ZeroDivisionError:
+        divide = None
+    except FloatingPointError:
+        divide = None
+    finally:
+        print("Inside result: {}".format(divide))
+    return(divide)
+
