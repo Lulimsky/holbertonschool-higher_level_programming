20e26706ce4339807221f8488b19c5ed472d5d1f
lulimsky <3337@holbertonshool.com>
2022-01-18 21:08:49 +0000

    squareuno

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define class Square"""
+
+class Square:
+
+    def __init__(self, size):
+        """Size instance o a square with
+        no type either value verification, 
+        given as a private attribute"""
+        self.__size = size
