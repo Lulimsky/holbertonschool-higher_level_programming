33543b46b1bdafe2519ad8f3da8b83bd3ad481a0
lulimsky <3337@holbertonshool.com>
2022-01-18 23:41:15 +0000

    s3.3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,5 +13,5 @@ class Square:
         self.__size = size
 
     def area(self):
-        """Return the current area of the square"""
+        """Returns the current square area"""
         return self.__size ** 2
