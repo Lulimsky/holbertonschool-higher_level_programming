b91e5274f245deb3a5fcfdf12a5128efff11c972
lulimsky <3337@holbertonshool.com>
2022-02-05 06:12:25 +0000

    10fixes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    return max(a_dictionary)
+    if a_dictionary is not None and len(a_dictionary) > 0:
+        return max(a_dictionary.items(), key=lambda i: i[1])[0]
