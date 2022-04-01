5b387e38555091542b6675fd437db479efc67315
lulimsky <3337@holbertonschool.com>
2022-04-01 09:44:03 -0700

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,13 @@
 """This module is from the class Base """
+
+
 class Base:
     """Class descrption """
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
-
         else:
             Base.__nb_objects = Base.__nb_objects + 1
-
             self.id = Base.__nb_objects
