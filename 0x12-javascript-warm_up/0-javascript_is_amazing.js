6c63a966d631c2a34b97ad2f8993f52c4e21a198
lulimsky <3337@holbertonschool.com>
2022-04-04 13:38:00 -0700

    t0

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//javascript scrip to print 
+//a string using "const" to declare
+//an immutale variable 
+//
+//console.log() is a function in JavaScript which
+//is used to print any kind of variables defined
+//before in it or to just print any message
+//that needs to be displayed to the user.
+
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
