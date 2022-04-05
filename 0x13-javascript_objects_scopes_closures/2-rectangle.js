6f308997a0f6eb8d4b757f774d84690a5f2533e9
lulimsky <3337@holbertonschool.com>
2022-04-05 14:28:31 -0700

    t2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// The constructor must takes 2 arguments w and h
+// If w or h is equal to 0 or not a positive integer, create an empty object
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+};
