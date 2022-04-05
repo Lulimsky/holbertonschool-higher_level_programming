d6b689a1bbdfae5ea12fa10c9eac4b99166e1376
lulimsky <3337@holbertonschool.com>
2022-04-05 13:15:51 -0700

    t0

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// empty class defined
+class Rectangle {
+}
+// "module.exports" is actually a property
+// of the module object in node.js
+// Main purpose is to achieve modular programming
+// to separate the functionality of a program into independent,
+// interchangeable modules, such that each contains everything
+// necessary to execute only one aspect of the desired functionality
+module.exports = Rectangle;
