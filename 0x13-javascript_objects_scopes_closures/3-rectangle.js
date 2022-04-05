c5cb7d7aa8fadb2a0d10d256d52b2f43990e9ad1
lulimsky <3337@holbertonschool.com>
2022-04-05 15:05:27 -0700

    t3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// The constructor must takes 2 arguments w and h
+// If w or h is equal to 0 or not a positive integer, create an empty object
+// same as task3 adding an instance method
+// called print() that prints the rectangle using the character X
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
