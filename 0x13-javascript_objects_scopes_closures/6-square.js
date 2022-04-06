a18af82bad7b41ddf10aeb5a721657f343fab7ab
lulimsky <3337@holbertonschool.com>
2022-04-06 16:49:58 -0700

    adv

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const square = require('./5-square');
 
+const square = require('./5-square');
 class Square extends square {
-  charPrint (c) {
-    if (!c) {
+  charPrint (character) {
+    if (!character) {
       this.print();
     } else {
       for (let i = 0; i < this.width; i++) {
-        console.log(c.repeat(this.width));
+        console.log(character.repeat(this.width));
       }
     }
   }
