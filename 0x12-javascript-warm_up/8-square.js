e41a5b788f9e711c36e0169e76c0190e1cf03af3
lulimsky <3337@holbertonschool.com>
2022-04-06 14:22:41 -0700

    t8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fill = Number(process.argv[2]);
+if (isNaN(fill)){
+  console.log('Missing size');
+  } else {
+    for(x = 0; x < fill; x++){
+     console.log('X'.repeat(fill));
+    }
+}
