3f81c88c9add00da24f889c1a8e21fafd4fefebd
lulimsky <3337@holbertonschool.com>
2022-04-06 14:34:02 -0700

    t8 corrections

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const fill = Number(process.argv[2]);
-if (isNaN(fill)){
+const fill = (Number(process.argv[2]));
+if (isNaN(fill)) {
   console.log('Missing size');
-  } else {
-    for(x = 0; x < fill; x++){
-     console.log('X'.repeat(fill));
-    }
+} else {
+  for (let x = 0; x < fill; x++) {
+    console.log('X'.repeat(fill));
+  }
 }
